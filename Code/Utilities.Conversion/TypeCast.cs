76383e6017c0a05c8012282b523dca76b295cfa4
vikesh bansal <vikesh@essorsolutions.com>
2018-02-15 22:50:58 +0530

    initial commit

---
+++ Code/Utilities.Conversion/TypeCast.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public static class TypeCast
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="val"></param>
+    /// <param name="alt"></param>
+    /// <returns></returns>
+    public static T ToType<T>(this object val, T alt = default(T)) where T : struct, IConvertible
+    {
+        try
+        {
+            if (val == null) return alt;
+            if (val is DBNull) return alt;
+            return (T)Convert.ChangeType(val, typeof(T));
+        }
+        catch
+        {
+            return alt;
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="val"></param>
+    /// <param name="alt"></param>
+    /// <returns></returns>
+    public static T? ToTypeOrNull<T>(this object val, T? alt = null) where T : struct, IConvertible
+    {
+        try
+        {
+            if (val != null && !(val is DBNull))
+            {
+                return (T)Convert.ChangeType(val, typeof(T));
+            }
+            return null;
+        }
+        catch
+        {
+            return alt;
+        }
+    }
+
+}
---
+++ Code/Utilities.Helper/StringHelper.cs
@@ -0,0 +1,15 @@
+﻿public static class StringHelper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static string ToFirstLetterCapitalize(this string value)
+    {
+        if (string.IsNullOrEmpty(value)) return value;
+        if (string.IsNullOrWhiteSpace(value)) return value;
+        var textInfo = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo;
+        return textInfo.ToTitleCase(value);
+    }
+}
